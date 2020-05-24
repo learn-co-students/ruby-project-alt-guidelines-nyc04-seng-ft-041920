c964664b999ec929c8c59dfcb5bb38a35fca735c
JonTDean <JonathanTomDean@Gmail.com>
2020-05-24 03:08:57 -0400

    created tables

--- Gemfile
+++ Gemfile
@@ -5,5 +5,6 @@ gem "activerecord", '~> 5.2'
 gem "sinatra-activerecord"
 gem "sqlite3", '~> 1.3.6'
 gem "pry"
+gem "rake"
 gem "require_all"
 gem "faker"
---
+++ db/migrate/20200524064912_create_recipe.rb
@@ -0,0 +1,9 @@
+class CreateRecipe < ActiveRecord::Migration[5.2]
+  def change
+    create_table :recipes do |t|
+      t.integer :user_id
+      t.string :title
+      t.string :category
+    end
+  end
+end
