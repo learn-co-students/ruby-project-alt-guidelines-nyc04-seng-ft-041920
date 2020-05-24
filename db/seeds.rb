27fbcfa271841fd678cdd9735c3e6b1d7559b55c
github username <github email address>
2020-05-24 18:38:17 -0400

    adding to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,8 @@ Ingredient.create(name: Faker::Food.unique.ingredient)
 
 #RECIPES
 Recipe.create(title: Faker::Food.unique.dish,  user_id: 1)
+Recipe.create(title: Faker::Food.unique.dish,  user_id: 2)
+
 
 #UNITS
 #SPLIT MEASUREMENT BY [0] = Amount :: [1] = name
@@ -22,4 +24,7 @@ Unit.create(name: split[1])
 binding.pry
 
 #ORDERS
-Order.create(1, 1, 1, split[0].to_f)
+Order.create(recipe_id: 1, ingredient_id: 2, unit_id: 1, amount: split[0].to_f)
+Order.create(recipe_id: 2, ingredient_id: 1, unit_id: 1, amount: split[0].to_f)
+Order.create(recipe_id: 1, ingredient_id: 1, unit_id: 1, amount: split[0].to_f)
+Order.create(recipe_id: 2, ingredient_id: 2, unit_id: 1, amount: split[0].to_f)
