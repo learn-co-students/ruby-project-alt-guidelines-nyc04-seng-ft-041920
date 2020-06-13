64daa61a351ad71775a39eb5a59b7c1559970f91
iamkevn <iamkevnrodriguez@gmail.com>
2020-06-13 12:21:52 -0400

    Fin

--- lib/models/interface.rb
+++ lib/models/interface.rb
@@ -50,6 +50,7 @@ sleep 1
                 self.create_account
             end
         password = prompt.ask("Password: ")
+        binding.pry
         sleep 1
         username = prompt.ask('Name: ')
         sleep 1
@@ -58,6 +59,7 @@ sleep 1
         address = prompt.ask('Current Location: ')   
         
         @user = User.create(name: username, emailaddress: email, phonenumber: phone_number, address: address, password: password)
+        binding.pry
         self.delivery
     end
 
